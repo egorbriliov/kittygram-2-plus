084a2d6f05a9ed13c19913e74bdee9d73a60c6f3
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2026-05-29 17:28:45 +0300

    Change object permission check for OwnerOrReadOnly: Currently NotAuth user can use SafeMethods.

--- cats/permissions.py
+++ cats/permissions.py
@@ -1,21 +1,29 @@
 """Модуль для прав взаимодействия с котиками."""
 
 from rest_framework import permissions
+from rest_framework.request import Request
+from rest_framework.views import APIView
 
 
 class OwnerOrReadOnly(permissions.BasePermission):
     """Класс который разрешает изменение для владелца и просмот для всех."""
 
-    def has_permission(self, request, view) -> bool:
+    def has_permission(self,
+                       request: Request,
+                       view: APIView) -> bool:
         """Проверяет чтобы пользователь был авторизирован на уровне запроса."""
         return (
                 request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated
             )
 
-    def has_object_permission(self, request, view, obj) -> bool:
+    def has_object_permission(self,
+                              request: Request,
+                              view: APIView, obj) -> bool:
         """Проверяет, чтобы пользователь был владельцем."""
-        return obj.owner == request.user
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request
 
 
 class ReadOnly(permissions.BasePermission):
