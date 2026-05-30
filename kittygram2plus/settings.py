bb2b5f156fca1f9f43edb41efdcfb2898684975f
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2026-05-30 14:26:00 +0300

    Fix key Name

--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -148,7 +148,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     # Пагинация для ограничения количества данных, возвращаемых в одном запросе
-    'default_pagination_class':
+    'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
 }
