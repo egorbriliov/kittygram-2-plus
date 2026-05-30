b269a1f6333bc807aa3e2c46b7fe169e62336752
Egor Briliov <88632284+egorbriliov@users.noreply.github.com>
2026-05-30 13:43:57 +0300

    Add view local trotling class.

--- cats/views.py
+++ cats/views.py
@@ -1,5 +1,6 @@
 """Отображения для взаимодейтсвия с котиками."""
 from rest_framework import viewsets
+from rest_framework.throttling import AnonRateThrottle
 
 from .models import Achievement, Cat, User
 from .permissions import OwnerOrReadOnly
@@ -12,6 +13,7 @@ class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
     permission_classes = [OwnerOrReadOnly]  # Базовый для всех операций.
+    trottle_classes = [AnonRateThrottle]
 
     def perform_create(self, serializer):
         """Метод добавления котиков."""
